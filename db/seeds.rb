9e2f92d504c260d08d79d734dfbd92edbd57053c
osciel morales <osci.morales.m@gmail.com>
2021-07-22 10:32:28 -0400

    Add article model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "faker"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(10).sample.times do
+  article = Article.new(
+    title: Faker::Commerce.product_name,
+    content: Faker::Lorem.paragraph_by_chars
+  )
+  article.save
+  puts article
+end
